689beb6029a1925415b9bbc2f493383e4a6ee2d1
CviPedro <135079780+CviPedro@users.noreply.github.com>
2025-06-02 20:24:59 -0300

    Update Quarto.java(2)

--- Quarto.java
+++ Quarto.java
@@ -1,3 +1,6 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Quarto {
     private String descQuarto;
     private int idQuarto;
@@ -20,4 +23,43 @@ public class Quarto {
     public void setIdQuarto(int idQuarto){
         this.idQuarto = idQuarto;
     }
+
+    @Override
+    public Boolean inserir(){
+        try {
+
+        } catch(IOException e) {
+            System.out.println("Falha ao encontrar arquivo "+ e.getMessage());
+            return false;
+        }
+        return true;
+    }
+    public Boolean editar(){
+        try {
+            
+        } catch (IOException e) {
+            System.out.println("Falha ao encontrar arquivo "+ e.getMessage());
+            return false;
+        }
+        return true;
+    }
+    public ArrayList<Pessoa> listar(){
+        try {
+            
+        } catch (IOException e) {
+            System.out.println("Falha ao encontrar arquivo "+ e.getMessage());
+
+        }
+    }
+    public int consultar(int id){
+        try {
+            
+        } catch (IOException e) {
+            System.out.println("Falha ao encontrar arquivo "+ e.getMessage());
+        }
+    }
+    public void mostrar(){
+       System.out.println(descQuarto + ";"+ idQuarto);
+    }
+
 }
