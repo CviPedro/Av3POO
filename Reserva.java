2b6f1b29a5a8844aaec5faeb8e5d281ee96a5543
CviPedro <135079780+CviPedro@users.noreply.github.com>
2025-06-02 20:24:30 -0300

    Update Reserva.java(2)

--- Reserva.java
+++ Reserva.java
@@ -1,3 +1,6 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Reserva { 
     private int idReserva;
     private Double valor;
@@ -20,40 +23,99 @@ public class Reserva {
         this.hospede = hospede;
     }
     public int getIdReserva(){
-        return this.idReserva;
+        return idReserva;
     }
     public void setIdReserva(int idReserva){
         this.idReserva = idReserva;
     }
 
     public Double getValor(){
-        return this.valor;
+        return valor;
     }
     public void getValor(Double valor){
         this.valor = valor;
     }
 
     public String getDataEntrada(){
-        return this.dataEntrada;
+        return dataEntrada;
     }
     public void setDataEntrada(String dataEntrada){
         this.dataEntrada = dataEntrada;
     }
 
     public String getDataSaida(){
-        return this.dataSaida;
+        return dataSaida;
     }
     public void setDataSaida(String dataSaida){
         this.dataSaida = dataSaida;
     }
 
     public Boolean getCheckin(){
-        return this.checkin;
+        return checkin;
     }
     public void setCheckin(Boolean checkin){
-        this
+        this.checkin = checkin;
     }
 
+    public Boolean getCheckout(){
+        return checkout;
+    }
+    public void setCheckout(Boolean checkout){
+        this.checkout = checkout;
+    }
+
+    public Quarto getquarto(){
+        return quarto;
+    }
+    public void setQuarto(Quarto quarto){
+        this.quarto = quarto;
+    }
+    public Hospede getHospede(){
+        return hospede;
+    }
+    public void setHospede(Hospede hospede){
+        this.hospede = hospede;
+    }
+
+     @Override
+    public Boolean inserir(){
+        try {
+
+        } catch(IOException e) {
+            System.out.println("Falha ao encontrar arquivo "+ e.getMessage());
+
+            return false;
+        }
+        return true;
+    }
+    public Boolean editar(){
+        try {
+            
+        } catch (IOException e) {
+            System.out.println("Falha ao encontrar arquivo "+ e.getMessage());
+            return false;
+        }
+        return true;
+    }
+    public ArrayList<Pessoa> listar(){
+        try {
+            
+        } catch (Exception e) {
+            System.out.println("Falha ao encontrar arquivo "+ e.getMessage());
+
+        }
+    }
+    public int consultar(int id){
+        try {
+            
+        } catch (IOException e) {
+            System.out.println("Falha ao encontrar arquivo "+ e.getMessage());
+        }
+    }
+    public void mostrar(){
+        System.out.println(idReserva+ ";"+ valor+ ";"+ dataEntrada+ ";"+ dataSaida+ 
+        ";"+checkin+ ";"+ checkout+ ";"+ quarto+ ";"+ hospede );
+    }
 
     
 }
