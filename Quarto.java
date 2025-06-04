68a9ff1f788b11a071f24c4b5c7df51e28bcfb5b
CviPedro <135079780+CviPedro@users.noreply.github.com>
2025-06-03 22:32:39 -0300

    Update Quarto.java(3)

--- Quarto.java
+++ Quarto.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -24,16 +28,23 @@ public class Quarto {
         this.idQuarto = idQuarto;
     }
 
-    @Override
     public Boolean inserir(){
-        try {
-
-        } catch(IOException e) {
-            System.out.println("Falha ao encontrar arquivo "+ e.getMessage());
+        
+        try{
+            FileWriter fw = new FileWriter("Quarto.txt", true);
+            
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(descQuarto+ ";"+idQuarto );
+            bw.close();
+            fw.close();
+        }catch(IOException e){
+            System.out.println(e.getMessage());
             return false;
         }
         return true;
     }
+
+
     public Boolean editar(){
         try {
             
@@ -43,21 +54,44 @@ public class Quarto {
         }
         return true;
     }
-    public ArrayList<Pessoa> listar(){
-        try {
-            
-        } catch (IOException e) {
-            System.out.println("Falha ao encontrar arquivo "+ e.getMessage());
 
-        }
-    }
-    public int consultar(int id){
-        try {
+    public ArrayList<Quarto> listar(){
+        ArrayList<Quarto> quartos = new ArrayList<>();
+        try{
+            FileReader fr = new FileReader("Quarto.txt");
             
-        } catch (IOException e) {
-            System.out.println("Falha ao encontrar arquivo "+ e.getMessage());
+            BufferedReader br = new BufferedReader(fr);
+            String linha;
+            String[] dados;
+            while((linha = br.readLine()) != null){
+                dados = linha.split(";");
+            Quarto q = new Quarto(dados[0], Integer.parseInt(dados[1]));
+               quartos.add(q);
+            }
+            br.close();
+        }catch(IOException e){
+            System.out.println(e.getMessage());
         }
+        return quartos;
     }
+
+    public Quarto consultar(int id){
+        Quarto retorno = null;
+        try{
+            for(Quarto q: listar()){
+                if(q.idQuartao == (id)){
+                    retorno = q;
+                    break;
+                }
+            }
+                return retorno;
+            }catch(Exception e){
+                System.out.println(e.getMessage());
+                return retorno;
+            }
+            
+    }
+
     public void mostrar(){
        System.out.println(descQuarto + ";"+ idQuarto);
     }
