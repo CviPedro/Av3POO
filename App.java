3b08c2dae3ec63ea968a1f0045ff1843b70eb648
CviPedro <135079780+CviPedro@users.noreply.github.com>
2025-06-05 20:47:13 -0300

    App.java (enviado)

--- App.java
+++ App.java
@@ -1,41 +1,34 @@
+
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        int escolha;
-        System.out.println("----- Menu principal -----");
-        System.out.print("\n Escolha uma das opções: ");
-        System.out.println("1 - Gerenciar Funcionario");
-        System.out.println("2 - Gerenciar Hospede");
-        System.out.println("3 - Gerenciar Quarto");
-        System.out.println("4 - Gerenciar Produto");
-        System.out.println("5 - Gerenciar Reserva");
-
-        escolha = sc.nextInt();
-        sc.nextLine();
-
-        switch(escolha){
-            case 1:
-                Util.gerenciarFuncionario();
-                break;
-            case 2:
-                Util.gerenciarHospede();
-                break;
-            case 3:
-                Util.gerenciarQuarto();
-                break;
-            case 4:
-                Util.gerenciarProduto();
-                break;
-            case 5:
-                Util.gerenciarReserva();
-                break;
-            case 6:
-                System.out.println("Fechando do sistema...");
-                sc.close(); 
-                return;
-            default:
-            System.out.println("Opção inválida, tente novamente!");
-
+        try (Scanner sc = new Scanner(System.in)) {
+            int escolha;
+            System.out.println("----- Menu principal -----");
+            System.out.println("1 - Gerenciar Funcionario");
+            System.out.println("2 - Gerenciar Hospede");
+            System.out.println("3 - Gerenciar Quarto");
+            System.out.println("4 - Gerenciar Produto");
+            System.out.println("5 - Gerenciar Reserva");
+            System.out.print(" Escolha uma das opções: ");
+            escolha = sc.nextInt();
+            sc.nextLine();
+            
+            switch(escolha){
+                case 1 -> Utilidade.gerenciarFuncionario();
+                case 2 -> Utilidade.gerenciarHospede();
+                case 3 -> Utilidade.gerenciarQuarto();
+                case 4 -> Utilidade.gerenciarProduto();
+                case 5 -> Utilidade.gerenciarReserva();
+                case 6 -> {
+                    System.out.println("Fechando do sistema...");
+                    sc.close();
+                    return;
+                }
+                default -> System.out.println("Opção inválida, tente novamente!");
+                
+            }
         }
         
     }
